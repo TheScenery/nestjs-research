bd611e24a7bb82d70278583ebe63ba9c75ae59d9
TheScenery <liuwenbin.lwb@gmail.com>
2023-12-19 22:30:22 +0800

    test _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  console.log(router.pathname);
+
+  if (typeof window !== "undefined" && router.pathname === "/") {
+    router.push("/ssr");
+    return null;
+  }
+
   console.log("APP render");
   return <Component {...pageProps} />;
 }
